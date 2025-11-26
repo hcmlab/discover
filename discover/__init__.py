0b93094dada2f5b1da5b082a0627a0b1ad7f3c56
saveli <abhaltenimlos@gmail.com>
2025-11-26 15:17:58 +0100

    Bump version to 1.1.0

--- discover/__init__.py
+++ discover/__init__.py
@@ -8,8 +8,8 @@ Date:
 """
 # We follow Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = '1'
-_MINOR_VERSION = '0'
-_PATCH_VERSION = '7'
+_MINOR_VERSION = '1'
+_PATCH_VERSION = '0'
 
 __version__ = '.'.join([
     _MAJOR_VERSION,
